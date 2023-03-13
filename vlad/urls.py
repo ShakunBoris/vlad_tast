cd9a13e6df92cc17ccc47a98bbb87f11b88c28b1
ShakunBoris <105981018+ShakunBoris@users.noreply.github.com>
2023-03-13 21:08:12 +0000

    qwe

--- vlad/urls.py
+++ vlad/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('save', views.save, name='save'),
+    path('show', views.show, name='show'),
 ]
--- vlad/views.py
+++ vlad/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core import serializers
 
 # Create your views here.
 def index(request):
-    cur = Currency.objects.all()
-    return render(request, 'vlad/index.html', {'cur': cur})
+    # cur = Currency.objects.all()
+    # {'cur': cur}
+    return render(request, 'vlad/index.html', {})
 
-def put
+@csrf_exempt
+def save(request):
+    if request.method == "POST":
+        Currency.objects.all().delete()
+        # print(json.loads(request.body))
+        for k, v in json.loads(request.body).items():
+            # print(k, v)
+            x = Currency(cur=k, value=v)
+            x.save()
+    return JsonResponse({'status': 'works'})
+
+@csrf_exempt
+def show(request):
+    if request.method == 'GET':
+        # print(list(Currency.objects.all().values('cur', 'value')))
+        cur = list(Currency.objects.all().values('cur', 'value'))
+        # cur = serializers.serialize('json', Currency.objects.all().values('cur', 'value'))
+        # print(cur)
+        return JsonResponse(cur, safe=False)
